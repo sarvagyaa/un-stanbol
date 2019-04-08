4b9570ba36e0ff951578c492028e4c01618e55a1
Mido22 <ban.mido22@gmail.com>
2019-04-08 13:25:10 +0530

    publish page

---
+++ docs/precache-manifest.a925261c8e784daab5c2f7796d9a765f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b590aed43a02a1fcdfc",
+    "url": "/Mido22/un-stanbol/static/js/runtime~main.3311b6c9.js"
+  },
+  {
+    "revision": "3c493e164a7ccb7f41d3",
+    "url": "/Mido22/un-stanbol/static/js/main.ab2191cd.chunk.js"
+  },
+  {
+    "revision": "be9d1052834d58f2b57e",
+    "url": "/Mido22/un-stanbol/static/js/2.857262f4.chunk.js"
+  },
+  {
+    "revision": "3c493e164a7ccb7f41d3",
+    "url": "/Mido22/un-stanbol/static/css/main.01c03b4f.chunk.css"
+  },
+  {
+    "revision": "395715b1b309283c2019a4f96bb40c67",
+    "url": "/Mido22/un-stanbol/index.html"
+  }
+];
