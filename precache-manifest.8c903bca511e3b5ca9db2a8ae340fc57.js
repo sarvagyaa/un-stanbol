ca90b9e79fd0d5f6fe2c652632649b3f8b9e6916
Abhimanyu Sarvagyam <abhi.manyu@me.com>
2019-04-10 18:10:10 +0800

    Updates

---
+++ precache-manifest.8c903bca511e3b5ca9db2a8ae340fc57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fb3314b2c2ba435d8b5",
+    "url": "/un-stanbol/static/js/runtime~main.f62f44fa.js"
+  },
+  {
+    "revision": "2c8d0dd72d9ac6c0320b",
+    "url": "/un-stanbol/static/js/main.81fbea4f.chunk.js"
+  },
+  {
+    "revision": "ce2a459ec8770c4bf5c9",
+    "url": "/un-stanbol/static/js/2.b43870c4.chunk.js"
+  },
+  {
+    "revision": "2c8d0dd72d9ac6c0320b",
+    "url": "/un-stanbol/static/css/main.8ad9ecde.chunk.css"
+  },
+  {
+    "revision": "7aa4c83c0adc51379780ca4dcff0cafd",
+    "url": "/un-stanbol/index.html"
+  }
+];
